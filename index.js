d317f9156ce12a5406f56232f7e0f954cc459f8f
Jenina <jeninadelacruz521@yahoo.com>
2024-04-15 23:27:12 -0700

    added test and js file contents

---
+++ index.js
@@ -0,0 +1,44 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+const {Circle, Triangle, Square} = require('./lib/shapes')
+const Svg = require('./lib/svg.js')
+
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape for your logo?',
+        choices: ['circle', 'triangle', 'square']
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'What color is the shape',
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What text would you want?',
+        validate: text => text.length <= 3 || 'not more than three characters'
+    },
+    {
+        type: 'input',
+        name: 'fontcolor',
+        message: 'What color do you want for the text?',
+    }
+])
+.then(answers => {
+    let shape;
+    if(answers.shape ==="circle")shape = new Circle()
+    else if(answers.shape === "triangle")shape = new Triangle()
+    else shape = new Square()
+
+    shape.setColor(answers.color)
+    const svg = new Svg()
+    svg.setText(answers.text, answers.fontcolor)
+    svg.setShape(shape)
+    fs.writeFile('logo.svg', svg.render(), err =>{
+        if(err)console.log(err)
+        else console.log('log successfully created!')
+    })
+})
